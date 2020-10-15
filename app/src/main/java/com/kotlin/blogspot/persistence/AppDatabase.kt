4764cba241dff32aac1de315de528c52a12f86bf
Jhon Jherick Maravilla <maravijh@sheridancollege.ca>
2020-10-14 23:21:54 -0400

    Saving Database setup

--- app/src/main/java/com/kotlin/blogspot/persistence/AppDatabase.kt
+++ app/src/main/java/com/kotlin/blogspot/persistence/AppDatabase.kt
@@ -1,5 +1,19 @@
 package com.kotlin.blogspot.persistence
 
-abstract class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.kotlin.blogspot.model.AccountProperties
+import com.kotlin.blogspot.model.AuthToken
+
+@Database(entities = [AuthToken::class, AccountProperties::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+
+    companion object{
+        val DATABASE_NAME: String = "app_db"
+    }
 
 }
--- app/src/main/java/com/kotlin/blogspot/persistence/AuthTokenDao.kt
+++ app/src/main/java/com/kotlin/blogspot/persistence/AuthTokenDao.kt
@@ -1,4 +1,18 @@
 package com.kotlin.blogspot.persistence
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.kotlin.blogspot.model.AuthToken
+
+@Dao
 interface AuthTokenDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(authToken: AuthToken): Long
+
+    @Query("UPDATE auth_token SET token = null WHERE account_pk = :pk")
+    fun nullifyToken(pk: Int): Int
+
 }
